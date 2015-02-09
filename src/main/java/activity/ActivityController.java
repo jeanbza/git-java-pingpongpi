fc276f7ad92c71a78bba5a63df5078731d2828b9
Jean de Klerk <jadekler@gmail.com>
2015-02-09 12:50:20 -0700

    wip: getting db connection working. connecting directly to db is temporary and will be ripped. not implemented in production (username + password dont exist)

--- src/main/java/activity/ActivityController.java
+++ src/main/java/activity/ActivityController.java
@@ -2,11 +2,16 @@ package activity;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.apache.log4j.Logger;
 import org.springframework.http.*;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ActivityController {
+    static Logger logger = Logger.getLogger(ActivityController.class.getName());
+
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping(value = "/activity", method = RequestMethod.GET)
@@ -16,6 +21,74 @@ public class ActivityController {
 
     @RequestMapping(value = "/activity", method = RequestMethod.POST)
     public ResponseEntity<String> greeting(@RequestParam(value = "active") boolean activity) {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass(org.h2.Driver.class);
+        dataSource.setUsername("root");
+        dataSource.setUrl("localhost");
+        dataSource.setPassword("");
+
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+
+        logger.info(dataSource);
+        logger.info(jdbcTemplate);
+
+        jdbcTemplate.update("INSERT INTO spring_test.activity VALUES 1");
+
         return new ResponseEntity<String>(HttpStatus.OK);
     }
 }
+
+/*
+package hello;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+public class Application {
+
+    public static void main(String args[]) {
+        // simple DS for test (not for production!)
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass(org.h2.Driver.class);
+        dataSource.setUsername("sa");
+        dataSource.setUrl("jdbc:h2:mem");
+        dataSource.setPassword("");
+
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+
+        System.out.println("Creating tables");
+        jdbcTemplate.execute("drop table customers if exists");
+        jdbcTemplate.execute("create table customers(" +
+                "id serial, first_name varchar(255), last_name varchar(255))");
+
+        String[] names = "John Woo;Jeff Dean;Josh Bloch;Josh Long".split(";");
+        for (String fullname : names) {
+            String[] name = fullname.split(" ");
+            System.out.printf("Inserting customer record for %s %s\n", name[0], name[1]);
+            jdbcTemplate.update(
+                    "INSERT INTO customers(first_name,last_name) values(?,?)",
+                    name[0], name[1]);
+        }
+
+        System.out.println("Querying for customer records where first_name = 'Josh':");
+        List<Customer> results = jdbcTemplate.query(
+                "select * from customers where first_name = ?", new Object[] { "Josh" },
+                new RowMapper<Customer>() {
+                    @Override
+                    public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
+                        return new Customer(rs.getLong("id"), rs.getString("first_name"),
+                                rs.getString("last_name"));
+                    }
+                });
+
+        for (Customer customer : results) {
+            System.out.println(customer);
+        }
+    }
+}
+ */
