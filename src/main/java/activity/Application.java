889b918b54c2940846bafb4551c0ae5908a3de3d
Jean de Klerk <jadekler@gmail.com>
2015-02-09 21:03:58 -0700

    wip commit towards db connection - successful mysql connect, bad method

--- src/main/java/activity/Application.java
+++ src/main/java/activity/Application.java
@@ -2,11 +2,34 @@ package activity;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.*;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
 
 @ComponentScan
 @EnableAutoConfiguration
 public class Application {
+    @Bean
+    DataSource dataSource() throws SQLException {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriver(new com.mysql.jdbc.Driver());
+        dataSource.setUrl("jdbc:mysql://localhost/spring_test");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+
+        return dataSource;
+    }
+
+    @Bean
+    JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        jdbcTemplate.update("insert into activity(id, active) values (?, ?)", 5, 1);
+        return jdbcTemplate;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
