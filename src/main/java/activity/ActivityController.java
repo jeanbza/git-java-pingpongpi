fb7b1de960d0d4b7aea5f451fcfe7d849c692eff
Jean de Klerk <jadekler@gmail.com>
2015-02-12 22:40:44 -0700

    Flesh out tests for activity controller

--- src/main/java/activity/ActivityController.java
+++ src/main/java/activity/ActivityController.java
@@ -1,35 +1,46 @@
 package activity;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.*;
-import org.springframework.jdbc.core.*;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import javax.servlet.http.*;
+import java.io.*;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static java.text.MessageFormat.format;
 
 @RestController
 public class ActivityController {
+    private final ObjectMapper jsonMapper = new ObjectMapper();
     private final ActivityDAO activityDAO;
 
-    @Autowired
     public ActivityController(ActivityDAO activityDAO) {
         this.activityDAO = activityDAO;
     }
 
     @RequestMapping(value = "/activity", method = RequestMethod.GET)
-    public ResponseEntity<String> greeting() {
+    public ResponseEntity<String> activity() {
         List<Activity> activityList = activityDAO.getActivities();
-        String json = "[{" + activityList.stream().map(activity -> format("\"active\":{0},\"created_at\":\"{1}\"", activity.isActive(), activity.getCreatedAt())).collect(Collectors.joining("},{")) + "}]";
+
+        String json = "[{" + activityList.stream()
+            .map(activity -> format("\"active\":{0},\"created_at\":\"{1}\"", activity.isActive(), activity.getCreatedAt()))
+            .collect(Collectors.joining("},{")) + "}]";
 
         return new ResponseEntity<>(json, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/activity", method = RequestMethod.POST)
-    public ResponseEntity<String> greeting(@RequestParam(value = "active") boolean active) {
-        activityDAO.addActivity(active);
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity<String> activity(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Map<String, Boolean> activityRequest = jsonMapper.readValue(request.getReader(), HashMap.class);
+
+        if (activityRequest.containsKey("active")) {
+            activityDAO.addActivity(activityRequest.get("active"));
+
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }
+
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 }
--- src/test/java/activity/ActivityControllerTest.java
+++ src/test/java/activity/ActivityControllerTest.java
@@ -7,18 +7,17 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.time.LocalDateTime;
 
 import static java.util.Arrays.asList;
-import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.initMocks;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 public class ActivityControllerTest {
-    @InjectMocks
-    ActivityController controller;
-    @Mock
-    ActivityDAO activityDAO;
+    @InjectMocks ActivityController controller;
+    @Mock ActivityDAO activityDAO;
 
     private MockMvc mockMvc;
 
@@ -35,8 +34,27 @@ public class ActivityControllerTest {
             new Activity(true, LocalDateTime.parse("2015-01-01T00:00:05"))
         )).when(activityDAO).getActivities();
 
+        String expectedContent = "[{\"active\":false,\"created_at\":\"2015-01-01T00:00:03\"},{\"active\":true,\"created_at\":\"2015-01-01T00:00:05\"}]";
         mockMvc.perform(get("/activity"))
             .andExpect(status().isOk())
-            .andExpect(content().string("[{\"active\":false,\"created_at\":\"2015-01-01T00:00:03\"},{\"active\":true,\"created_at\":\"2015-01-01T00:00:05\"}]"));
+            .andExpect(content().string(expectedContent));
+    }
+
+    @Test
+    public void testPost() throws Exception {
+        Mockito.doNothing().when(activityDAO).addActivity(true);
+
+        mockMvc.perform(post("/activity").content("{\"active\":true}"))
+            .andExpect(status().isCreated());
+
+        verify(activityDAO).addActivity(true);
+    }
+
+    @Test
+    public void testPost_withBadRequest() throws Exception {
+        mockMvc.perform(post("/activity").content("{\"boom\":true}"))
+            .andExpect(status().isBadRequest());
+
+        verifyZeroInteractions(activityDAO);
     }
 }
