657d95d4c522da1eedd76a47b5f2b00d2ff3db74
Jean de Klerk <jadekler@gmail.com>
2015-03-01 14:45:21 -0700

    Working scatter plot (for the most part...ranges still need figuring out)

--- src/main/webapp/js/timeline.js
+++ src/main/webapp/js/timeline.js
@@ -1,32 +1,5 @@
 $("#scatter-load").empty();
 
-var hourlyData = [
-    {"hour": 1,  "lowerCount": 15, "upperCount": 5},
-    {"hour": 2,  "lowerCount": 19, "upperCount": 9},
-    {"hour": 3,  "lowerCount": 18, "upperCount": 8},
-    {"hour": 4,  "lowerCount": 18, "upperCount": 8},
-    {"hour": 5,  "lowerCount": 19, "upperCount": 9},
-    {"hour": 6,  "lowerCount": 17, "upperCount": 7},
-    {"hour": 7,  "lowerCount": 33, "upperCount": 13},
-    {"hour": 8,  "lowerCount": 31, "upperCount": 11},
-    {"hour": 9,  "lowerCount": 19, "upperCount": 9},
-    {"hour": 10,  "lowerCount": 14,  "upperCount": 4},
-    {"hour": 11,  "lowerCount": 18,  "upperCount": 8},
-    {"hour": 12,  "lowerCount": 17,  "upperCount": 7},
-    {"hour": 13,  "lowerCount": 17,  "upperCount": 7},
-    {"hour": 14,  "lowerCount": 18,  "upperCount": 8},
-    {"hour": 15,  "lowerCount": 17,  "upperCount": 7},
-    {"hour": 16,  "lowerCount": 16,  "upperCount": 6},
-    {"hour": 17,  "lowerCount": 25,  "upperCount": 15},
-    {"hour": 18,  "lowerCount": 33,  "upperCount": 13},
-    {"hour": 19,  "lowerCount": 17,  "upperCount": 7},
-    {"hour": 20,  "lowerCount": 24,  "upperCount": 14},
-    {"hour": 21,  "lowerCount": 14,  "upperCount": 4},
-    {"hour": 22,  "lowerCount": 11,  "upperCount": 1},
-    {"hour": 23,  "lowerCount": 19,  "upperCount": 9},
-    {"hour": 24,  "lowerCount": 2,  "upperCount": 10}
-];
-
 function renderCharts(data) {
     var margin = {top: 20, right: 20, bottom: 30, left: 40},
         width = 960 - margin.left - margin.right,
@@ -49,17 +22,13 @@ function renderCharts(data) {
     var seriesNames = d3.keys(data[0])
         .filter(function (d) {
             return d !== "hour";
-        })
-        .sort();
+        });
 
     var series = seriesNames.map(function (series) {
         return data.map(function (d) {
             return {x: +d.hour, y: +d[series]};
         });
     });
-
-    console.dir(seriesNames);
-    console.dir(series);
 
     x.domain(d3.extent(d3.merge(series), function (d) {
         return d.x;
@@ -82,7 +51,7 @@ function renderCharts(data) {
         .enter().append("g")
         .attr("class", "series")
         .style("fill", function (d, i) {
-            return z(i);
+            return i % 2 == 0 ? "green" : "red";
         })
         .selectAll(".point")
         .data(function (d) {
@@ -99,6 +68,35 @@ function renderCharts(data) {
         });
 }
 
+function normalizeDailyActivities(dailyActivities) {
+    var normalized = [];
+
+    for (var hour = 0; hour < 24; hour++) {
+        normalized.push({"hour": hour});
+    }
+
+    for (var dailyActivity of dailyActivities) {
+        for (var hour = 0; hour < 24; hour++) {
+            normalized[hour][dailyActivity.date + "_active"] = [];
+            normalized[hour][dailyActivity.date + "_inactive"] = [];
+        }
+
+        for (var hour = 0; hour < 24; hour++) {
+            normalized[hour][dailyActivity.date+"_active"].push(dailyActivity.hourlyActive[hour]);
+            normalized[hour][dailyActivity.date+"_inactive"].push(dailyActivity.hourlyInactive[hour]);
+        }
+    }
+
+    return normalized;
+}
+
 $(document).ready(function () {
-    renderCharts(hourlyData);
+    $.ajax({
+        url: "/pingpong/dailyActivity",
+        success: function(data) {
+            var jsonData = JSON.parse(data);
+            var normalizedJsonData = normalizeDailyActivities(jsonData);
+            renderCharts(normalizedJsonData);
+        }
+    });
 });
